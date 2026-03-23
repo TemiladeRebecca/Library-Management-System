3bf2b6920c6ee6a12aa5e9116d18874ca3abd04a
TemiladeRebecca <temilade.rebecca20@gmail.com>
2026-03-23 17:50:48 +0100

    Add new interface IBorrowable to two methods Borrow nd Return

---
+++ Library Mangement System/IBorrowable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library_Mangement_System
+{
+    public interface IBorrowable
+    {
+         void Borrow(string memberId);
+        void Return();
+    }
+}
