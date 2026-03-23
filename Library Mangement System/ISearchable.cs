6d9a3638bdccdbeffed2d0aa4de1397cb5319aa8
TemiladeRebecca <temilade.rebecca20@gmail.com>
2026-03-23 17:47:52 +0100

    Add new struct Isearchable that implements the method Search

---
+++ Library Mangement System/ISearchable.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library_Mangement_System
+{
+    public interface ISearchable
+    {
+         string Search();
+    }
+}
