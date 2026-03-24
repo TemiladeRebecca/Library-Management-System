ad9bac635cdfdecee804debdeeca05c10dde9127
TemiladeRebecca <temilade.rebecca20@gmail.com>
2026-03-24 19:07:21 +0100

    Integrate RedisDatabase into Program.cs

--- Library Mangement System/Program.cs
+++ Library Mangement System/Program.cs
@@ -6,41 +6,16 @@ namespace Library_Management_System
     {
         static void Main(string[] args)
         {
-            //List<Person> list = new List<Person>
-            //{
-            //    new Member("2", "Bob", "Premium"),
-            //    new Librarian("3", "Charlie", "EMP123")
-            //};
-
-            //foreach (var person in list)
-            //{
-            //    Console.WriteLine(person.GetDetails());
-            //}
-
-            // Wire up the dependency graph manually
-            IDatabase database = new InMemoryDatabase();
+            IDatabase database = new RedisDatabase("localhost:6379");
             INotifier notifier = new ConsoleNotifier();
             IBookRepository books = new BookRepository(database);
             IMemberRepository members = new MemberRepository(database);
             IBorrowRecordRepository borrowRecords = new BorrowRecordRepository(database);
-            ILibraryService service = new LibraryService(books, members, borrowRecords, notifier);
+            ILibraryService service = new LibraryService(database, books, members, borrowRecords, notifier);
             IMenuHandler menu = new MenuHandler(service, notifier);
 
             menu.Run();
 
         }
-
-
-        public interface IBorrowable
-        {
-            void Borrow(string memberId);
-            void Return();
-        }
-
-        public interface ISearchable
-        {
-            string Search();
-        }
-
     }
 }
